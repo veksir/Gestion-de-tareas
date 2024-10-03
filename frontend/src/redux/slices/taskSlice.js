8798975c18bc645e4bb7d079d1a74d9d1fbdb17d
Kevin Tobias Sena Molina <kevinsena243@gmail.com>
2024-10-02 22:38:13 -0500

    mas slices

--- frontend/src/redux/slices/taskSlice.js
+++ frontend/src/redux/slices/taskSlice.js
@@ -54,3 +54,91 @@ export const updateTask = createAsyncThunk(
         }
     }
 );
+
+// thunk para eliminar una tarea
+export const deleteTask = createAsyncThunk(
+    'tasks/deleteTask',
+    async (id, { getState, isRejectedWithValue }) => {
+        const { auth: { userInfo } } = getState();
+        try {
+            await axios.delete('https://localhost:5000/api/tareas/${id}', {
+                headers: {
+                    Authorization: 'Bearer ${userInfo.token}',
+                },
+            });
+            return id;
+        } catch (error) {
+            return isRejectedWithValue(error.response.data.mensaje);
+        }
+    }
+);
+
+// Slice de Tareas
+const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+        tareas: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: {
+        //obtener Tareas
+        [fetchTasks.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [fetchTasks.fullfiled]: (state, action) => {
+            state.loading = false;
+            state.tareas = action.payload;
+        },
+        [fetchTasks.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        // Crear Tarea
+        [fetchTasks.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [fetchTasks.fullfiled]: (state, action) => {
+            state.loading = false;
+            state.tareas.unshift(action.payload);
+        },
+        [fetchTasks.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        // Actualizar Tarea
+        [updateTask.pending]: (state) =>{
+            state.loading = true;
+            state.error = null;
+        },
+        [updateTask.fulfilled]: (state, action) =>{
+            state.loading = false;
+            const index = state.tareas.findIndex(tarea => tarea._id === action.payload._id);
+            if (index !== -1) {
+                state.tareas[index] = action.payload;
+            }
+        },
+        [updateTask.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        // Eliminar Tarea
+        [deleteTask.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [deleteTask.fullfiled]: (state, action) => {
+            state.loading = false;
+            state.tareas = state.tareas.filter(tarea => tarea._id !== action.payload);
+        },
+        [deleteTask.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    },
+});
+
+export default taskSlice.reducer;
